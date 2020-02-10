089fdc616f81a5c6566c533e154f45eb79913672
Berk9626 <berkberkmen12@gmail.com>
2020-02-10 19:11:06 +0300

    çok basit projede core session kullanımı

---
+++ SessionProjesi/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SessionProjesi.Models;
+
+namespace SessionProjesi.Controllers
+{
+    public class HomeController : Controller
+    {
+         string SessionName = "_Name";
+         string SessionAge = "_Age";
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString(SessionName, "Jarvik");
+            HttpContext.Session.SetInt32(SessionAge, 24);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            ViewBag.Name = HttpContext.Session.GetString(SessionName);
+            ViewBag.Age = HttpContext.Session.GetInt32(SessionAge);
+            ViewData["Message"] = "Asp.Net Core !!!.";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SessionProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SessionProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
